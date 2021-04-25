47f19f6c2ea95dba3eb91dbc8c3468b0cf94e174
Leonita Shala <leonita.shala@stud.fh-campuswien.ac.at>
2021-04-25 22:10:12 +0200

    updatequiz

--- app/src/main/java/com/example/mad03_fragments_and_navigation/QuizEndFragment.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/QuizEndFragment.kt
@@ -1,27 +1,42 @@
 package com.example.mad03_fragments_and_navigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.mad03_fragments_and_navigation.databinding.FragmentQuizEndBinding
 
 
 class QuizEndFragment : Fragment() {
     private lateinit var binding: FragmentQuizEndBinding
 
+
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_quiz_end, container, false)
 
         // get score from navigation arguments
+        var score = arguments?.getInt("score").toString()
+        var maxScore = arguments?.getInt("maxScore").toString()
 
-        // show score
+        //show score
+        binding.textView5.text = "$score / $maxScore"
 
+        //button
+        binding.restartBtn.setOnClickListener { view : View ->
+            restartQuiz(view)
+        }
         return binding.root
+    }
+
+    private fun restartQuiz(v: View) {
+        v.findNavController().navigate(R.id.action_quizEndFragment_to_quizFragment)
     }
 }
--- app/src/main/java/com/example/mad03_fragments_and_navigation/QuizFragment.kt
+++ app/src/main/java/com/example/mad03_fragments_and_navigation/QuizFragment.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.mad03_fragments_and_navigation.databinding.FragmentQuizBinding
 import com.example.mad03_fragments_and_navigation.models.QuestionCatalogue
+//import com.example.mad03_fragments_and_navigation.models.QuizViewModel
 
 
 class QuizFragment : Fragment() {
 
     private lateinit var binding: FragmentQuizBinding
+    //private lateinit var viewModel: QuizViewModel
     private val questions = QuestionCatalogue().defaultQuestions    // get a list of questions for the game
     private var score = 0                                           // save the user's score
     private var index = 0                                           // index for question data to show
@@ -24,19 +29,53 @@ class QuizFragment : Fragment() {
         binding.questionsCount = questions.size
         binding.question = questions[index]
 
-        binding.btnNext.setOnClickListener {
-            nextQuestion()
+        binding.btnNext.setOnClickListener { view : View ->
+            nextQuestion(view)
         }
 
         return binding.root
     }
 
-    private fun nextQuestion(){
+    private fun nextQuestion(v: View){
+        val answer = binding.answerBox.checkedRadioButtonId
+
+
+        //Toast message if no answer is selected
+        if(answer == -1) {
+            Toast.makeText(activity, "Select an answer!",
+                    Toast.LENGTH_SHORT).show()
+            return
+        }
         // get selected answer
-        // check if is correct answer
+        // check if is the correct answer
         // update score
-        // check if there are any questions left
-            // show next question OR
+        if(answer == binding.answer1.id) {
+            if(questions.get(index).answers.get(0).isCorrectAnswer)
+                score++
+        }
+        if(answer == binding.answer2.id) {
+            if(questions.get(index).answers.get(1).isCorrectAnswer)
+                score++
+        }
+        if(answer == binding.answer3.id) {
+            if(questions.get(index).answers.get(2).isCorrectAnswer)
+                score++
+        }
+        if(answer == binding.answer4.id) {
+            if(questions.get(index).answers.get(3).isCorrectAnswer)
+                score++
+        }
+        // check if any questions are left
+        if(questions.size-1 > index) {
+            index++
+            binding.index = index
+            binding.question = questions[index]
+        }
+        else {
             // navigate to QuizEndFragment
+            val scoreBundle = bundleOf("score" to score, "maxScore" to questions.size)
+            v.findNavController().navigate(R.id.action_quizFragment_to_quizEndFragment, scoreBundle)
+        }
+
     }
 }
